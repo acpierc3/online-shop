867aac84dc7196c4559b30944a577ab0e94bbbd9
Adam Pierce <adam.pierc3@gmail.com>
2019-07-12 10:46:02 -0700

    adding sequelize

--- util/database.js
+++ util/database.js
@@ -1,13 +1,32 @@
-const mysql = require('mysql2');
 const PRIVATE = require('./database.priv.js')
 
-//create pool instead of individual connections to DB each time there is a query
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: PRIVATE.PASSWORD
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', PRIVATE.PASSWORD, {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-//use promise here to allow chaining after promise resolution
-module.exports = pool.promise();
+module.exports = sequelize;
+
+
+
+
+
+//::::::::::::::::::::::::::::::
+//::::::PRE-SEQUELIZE CODE::::::
+//::::::::::::::::::::::::::::::
+
+// const mysql = require('mysql2');
+// const PRIVATE = require('./database.priv.js')
+
+// //create pool instead of individual connections to DB each time there is a query
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: PRIVATE.PASSWORD
+// });
+
+// //use promise here to allow chaining after promise resolution
+// module.exports = pool.promise();
