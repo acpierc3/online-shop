9d977f2374cfd7cbfbffd6695a2a77e8ef333f29
Adam Pierce <adam.pierc3@gmail.com>
2019-07-15 12:18:00 -0700

    updating db communicating logic to use sequelize package

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,16 +2,28 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([data, metaData]) => {
-    res.render('shop/product-list', {
-      prods: data,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  })
-  .catch(err => {
-    console.log(err)
-  })
+  Product.findAll()
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+  // Product.fetchAll().then(([data, metaData]) => {
+  //   res.render('shop/product-list', {
+  //     prods: data,
+  //     pageTitle: 'All Products',
+  //     path: '/products'
+  //   });
+  // })
+  // .catch(err => {
+  //   console.log(err)
+  // })
 };
 
 exports.getProduct = (req, res, next) => {
@@ -31,16 +43,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(([data, metaData]) => {
-    res.render('shop/index', {
-      prods: data,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  })
-  .catch(err => {
-    console.log(err)
-  })
+  Product.findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+
+  //code before sequelize
+  // Product.fetchAll().then(([data, metaData]) => {
+  //   res.render('shop/index', {
+  //     prods: data,
+  //     pageTitle: 'Shop',
+  //     path: '/'
+  //   });
+  // })
+  // .catch(err => {
+  //   console.log(err)
+  // })
 };
 
 exports.getCart = (req, res, next) => {
