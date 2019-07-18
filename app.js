c370e5ee7cf2fe5a6c2e7077cdb49ed68b0fe8b4
Adam Pierce <adam.pierc3@gmail.com>
2019-07-18 12:03:01 -0700

    adding functionality and db backend for orders page

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -36,15 +38,21 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-//define relations in models / db
+//define RELATIONS in models / db
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 //the two above relations are redundant because they go both ways, you can use either - or
-User.hasOne(Cart)
+User.hasOne(Cart);
 Cart.belongsTo(User);
 //many to many relationship needs intermediary table defined
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+//
+Order.belongsTo(User);
+User.hasMany(Order);
+//
+Order.belongsToMany(Product, {through: OrderItem});
+// Product.belongsToMany(Order, {through: OrderItem});
 
 
 sequelize
