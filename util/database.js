46a98dcd4d0839472961f908227a287ee48fee57
Adam Pierce <adam.pierc3@gmail.com>
2019-07-22 10:34:51 -0700

    converting db from sql to mongo

--- util/database.js
+++ util/database.js
@@ -1,19 +1,32 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const PRIVATE = require('./database.priv.js')
+const PRIVATE = require('./database.priv.js');
+let _db;
 
+//method for initial connection to database, will stay connected
 const mongoConnect = callback => {
     MongoClient.connect(
-        'mongodb+srv://node:' +PRIVATE.MONGO_PASSWORD +'@online-shop-dkmzb.mongodb.net/test?retryWrites=true&w=majority'
+        'mongodb+srv://node:' +PRIVATE.MONGO_PASSWORD +'@online-shop-dkmzb.mongodb.net/shop?retryWrites=true&w=majority'
     )
         .then(client => {
             console.log('Connected!');
+            _db = client.db();
             callback(client);
         })
         .catch(err => {
             console.log(err);
+            throw err;
         });
 }
 
-module.exports = mongoConnect;
+//method to access already connected database
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
