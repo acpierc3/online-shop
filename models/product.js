3fe2b6603a012dd5792edd9e2888408f200aec33
Adam Pierce <adam.pierc3@gmail.com>
2019-07-23 10:41:52 -0700

    converting db from sql to mongo

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(_id);
+    this._id = _id ? new mongodb.ObjectId(_id) : null;
   }
 
   save() {
@@ -53,6 +53,19 @@ class Product {
       .then(product => {
         console.log("HERE YA PRODUCT",product);
         return product;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .deleteOne({_id: new mongodb.ObjectId(prodId)})
+      .then(result => {
+        console.log(result);
+        return result;
       })
       .catch(err => {
         console.log(err);
