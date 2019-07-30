5142994b1f4d7e8ed40c25845f7b7e483f0461b2
Adam Pierce <adam.pierc3@gmail.com>
2019-07-30 11:39:17 -0700

    updated show orders page and styling on that page

--- models/user.js
+++ models/user.js
@@ -135,10 +135,12 @@ class User {
     
   }
 
-  // getOrders() {
-  //   const db = getDb();
-  //   return db.collection('orders').
-  // }
+  getOrders() {
+    const db = getDb();
+    return db.collection('orders')
+      .find({'user._id': new mongodb.ObjectId(this._id)})
+      .toArray();
+  }
 
 }
 
