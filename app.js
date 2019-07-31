c7074e891063ad740d463f6d94b907eabcca8b61
Adam Pierce <adam.pierc3@gmail.com>
2019-07-31 11:28:07 -0700

    converting db to use mongoose instead of mongoconnect

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 const PRIVATE = require('./util/database.priv.js');
 
 const app = express();
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('5d389abb29cbd416ec3cfcf3')
-        .then(user => {
-            req.user = new User(user.name, user.email, user._id, user.cart);
-            next();
-        })
-        .catch(err => console.log(err));
-})
+// app.use((req, res, next) => {
+//     User.findById('5d389abb29cbd416ec3cfcf3')
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user._id, user.cart);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
