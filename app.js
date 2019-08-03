795c73674437e0fc038daa223a7f2b236dc491e6
Adam Pierce <adam.pierc3@gmail.com>
2019-08-03 11:16:55 -0700

    added connect-mongodb-session middleware to store sessions in db and handle session cookies

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const PRIVATE = require('./util/database.priv.js');
 
+const MONGODB_URI = 'mongodb+srv://node:' +PRIVATE.MONGO_PASSWORD +'@online-shop-dkmzb.mongodb.net/shop?w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,7 +28,12 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({secret: PRIVATE.SESSION_SECRET, resave: false, saveUninitialized: false})
+    session({
+        secret: PRIVATE.SESSION_SECRET, 
+        resave: false, 
+        saveUninitialized: false,
+        store: store
+    })
 );
 
 app.use((req, res, next) => {
@@ -40,7 +52,7 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://node:' +PRIVATE.MONGO_PASSWORD +'@online-shop-dkmzb.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
     .then(result => {
         User.findOne()
             .then(user => {
