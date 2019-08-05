fed204475e4443b51b0200318709e49a37dd5565
Adam Pierce <adam.pierc3@gmail.com>
2019-08-05 12:30:29 -0700

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ exports.postLogin = (req, res, next) => {
 
     User.findById('5d431da60f54ac4e98c52383')
     .then(user => {
-        //user here is full mongoose model object, including all helper methods
+        //user here is full mongoose model object, including all helper methods.
         req.session.isLoggedIn = true;
         req.session.user = user;
         //normally dont need to save session, but this allows us to add callback function so we are only redirected once session has been stored in DB (prevents potential race conditions)
